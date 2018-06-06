8210b9ac0cee01d8255f1958f6d247fdab1fabcc
Stephen Knox <knoxsp@gmail.com>
2018-06-06 23:23:37 +0100

    Remove copy-paste error

--- hydra_base/lib/service.py
+++ hydra_base/lib/service.py
@@ -107,6 +107,5 @@ def get_session_user(session_id, **kwargs):
     if hydra_session is not None:
         hydra_session.delete()
         hydra_session.save()
-        return hydra_session['user_id']
 
     return None
