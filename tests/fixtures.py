b9fa911de0808691eefd1033c6c9a403563c1665
Giovanni Basolu <giovanni.basolu@gmail.com>
2019-01-17 14:52:00 +0000

    added the fixture to add a second network on a secodn project

--- tests/fixtures.py
+++ tests/fixtures.py
@@ -97,6 +97,11 @@ def network_with_data(project_id=None, num_nodes=10, ret_full_net=True, new_proj
     return util.create_network_with_data(project_id, num_nodes, ret_full_net, new_proj, map_projection)
 
 @pytest.fixture()
+# Creates a second project with a new network and returns the network created
+def second_network_with_data(project_id=None, num_nodes=10, ret_full_net=True, new_proj=True, map_projection='EPSG:4326'):
+    return util.create_network_with_data(project_id, num_nodes, ret_full_net, new_proj, map_projection)
+
+@pytest.fixture()
 def network_with_extra_group(project_id=None, num_nodes=10, ret_full_net=True, new_proj=True, map_projection='EPSG:4326'):
     return util.create_network_with_extra_group(project_id, num_nodes, ret_full_net, new_proj, map_projection)
 
