71f3b90dab7e5b8b9ee1293c3a5b60d3c47abfd2
Giovanni Basolu <giovanni.basolu@gmail.com>
2019-03-06 14:54:35 +0000

    removed 3 relationships to unit and dimension

--- hydra_base/db/model.py
+++ hydra_base/db/model.py
@@ -119,7 +119,7 @@ class Dataset(Base, Inspect):
     value      = Column('value', Text().with_variant(mysql.TEXT(4294967295), 'mysql'),  nullable=True)
 
     user = relationship('User', backref=backref("datasets", order_by=id))
-    unit = relationship('Unit', backref=backref("dataset_unit", order_by=unit_id))
+    #####unit = relationship('Unit', backref=backref("dataset_unit", order_by=unit_id))
 
     _parents  = ['tResourceScenario', 'tUnit']
     _children = ['tMetadata']
@@ -369,7 +369,7 @@ class Attr(Base, Inspect):
     description  = Column(String(1000))
     cr_date = Column(TIMESTAMP(),  nullable=False, server_default=text(u'CURRENT_TIMESTAMP'))
 
-    dimensions = relationship('Dimension', backref=backref("attr_dimension", order_by=dimension_id))
+    #####dimensions = relationship('Dimension', backref=backref("attr_dimension", order_by=dimension_id))
 
     _parents  = ['tDimension']
     _children = []
@@ -509,7 +509,7 @@ class TypeAttr(Base, Inspect):
 
     attr = relationship('Attr')
     templatetype = relationship('TemplateType',  backref=backref("typeattrs", order_by=attr_id, cascade="all, delete-orphan"))
-    unit = relationship('Unit', backref=backref("typeattr_unit", order_by=unit_id))
+    ####unit = relationship('Unit', backref=backref("typeattr_unit", order_by=unit_id))
     default_dataset = relationship('Dataset')
 
     _parents  = ['tTemplateType', 'tUnit']
