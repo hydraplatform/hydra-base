a679dfde599a10fb8d9032d18508cb48d17477ce
Giovanni Basolu <giovanni.basolu@gmail.com>
2019-04-08 16:36:24 +0100

    removed forced commit

--- hydra_base/lib/units.py
+++ hydra_base/lib/units.py
@@ -392,7 +392,6 @@ def bulk_add_dimensions(dimension_list, **kwargs):
     for dimension in dimension_list:
         added_dimensions.append(add_dimension(dimension, **kwargs))
     log.info("hydra-base.units.bulk_add_dimensions - 2    ")
-    db.DBSession.commit()
 
     log.info(added_dimensions)
     return JSONObject({"dimensions": added_dimensions})
