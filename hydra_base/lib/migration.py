ccdbb2348c00c2c2093cea85517b7f54feeef796
Giovanni Basolu <giovanni.basolu@gmail.com>
2019-04-08 18:14:29 +0100

    added source and target url

--- hydra_base/lib/migration.py
+++ hydra_base/lib/migration.py
@@ -38,15 +38,16 @@ import logging
 log = logging.getLogger(__name__)
 
 #@required_perms("migrate_project")
-def get_project_for_migration(migration_name, source_project_id, **kwargs):
+def get_project_for_migration(migration_name, target_server_url, source_project_id, **kwargs):
     project_status = None
     try:
-        project_status = db.DBSession.query(MigrationStatus).filter(MigrationStatus.migration_name==migration_name).filter(MigrationStatus.source_project_id==source_project_id).one()
+        project_status = db.DBSession.query(MigrationStatus).filter(MigrationStatus.migration_name==migration_name).filter(MigrationStatus.target_server_url==target_server_url).filter(MigrationStatus.source_project_id==source_project_id).one()
     except NoResultFound:
         # The project has never been initialized does not exist
         project_status = MigrationStatus()
         project_status.migration_name = migration_name
         project_status.source_project_id = source_project_id
+        project_status.target_server_url = target_server_url
         project_status.target_project_id = None
         db.DBSession.add(project_status)
         db.DBSession.flush()
@@ -54,8 +55,8 @@ def get_project_for_migration(migration_name, source_project_id, **kwargs):
     return project_status
 
 #@required_perms("migrate_project")
-def set_target_project_id(migration_name, source_project_id, target_project_id,**kwargs):
-    project_status = get_project_for_migration(migration_name, source_project_id, **kwargs)
+def set_target_project_id(migration_name, target_url, source_project_id, target_project_id,**kwargs):
+    project_status = get_project_for_migration(migration_name, target_url, source_project_id, **kwargs)
 
     project_status.target_project_id = target_project_id
     db.DBSession.flush()
@@ -63,9 +64,9 @@ def set_target_project_id(migration_name, source_project_id, target_project_id,*
     return project_status
 
 #@required_perms("migrate_project")
-def add_network_to_project_status(migration_name, source_project_id, target_project_id, source_network_id, target_network_id, **kwargs):
+def add_network_to_project_status(migration_name, source_url, target_url, source_project_id, target_project_id, source_network_id, target_network_id, **kwargs):
     """
     """
-    project_status = set_target_project_id(migration_name, source_project_id, target_project_id,**kwargs)
-    project_status.add_network_done(network_data)
+    project_status = set_target_project_id(migration_name, target_url, source_project_id, target_project_id,**kwargs)
+    project_status.add_network_done({"source_network_id": source_network_id, "target_network_id": target_network_id})
     db.DBSession.flush()
