d061d2ad2464486398680a8ca2d8363da18b4276
Stephen Knox <stephen.knox@manchester.ac.uk>
2021-12-16 15:05:44 +0000

    Reinstate some removed code, taken away for testing

--- hydra_base/util/hdb.py
+++ hydra_base/util/hdb.py
@@ -489,12 +489,12 @@ def create_default_units_and_dimensions(update=True):
             else:
                 #log.critical("UNIT {}.{} EXISTANT".format(dimension_name,json_unit['abbr']))
                 pass
-    # try:
-    #     # Needed for test. on HWI it fails so we need to catch the exception and pass by
-    #     db.DBSession.commit()
-    # except Exception as e:
-    #     # Needed for HWI
-    #     pass
+    try:
+        # Needed for test. on HWI it fails so we need to catch the exception and pass by
+        db.DBSession.commit()
+    except Exception as e:
+        # Needed for HWI
+        pass
     return
 
 
