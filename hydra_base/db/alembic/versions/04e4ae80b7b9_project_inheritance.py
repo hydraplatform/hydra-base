a5585d248486ccb1d98dd939662b9e30d7d83ace
Stephen Knox <stephen.knox@manchester.ac.uk>
2022-04-06 10:10:22 +0100

    Add alembic script
    (cherry picked from commit 17ad47be5690b886c93ff5db8ef6b8fcffbeab8b)

---
+++ hydra_base/db/alembic/versions/04e4ae80b7b9_project_inheritance.py
@@ -0,0 +1,32 @@
+"""project-inheritance
+
+Revision ID: 04e4ae80b7b9
+Revises: cec2b77ad85e
+Create Date: 2022-01-10 10:00:02.614468
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '04e4ae80b7b9'
+down_revision = 'cec2b77ad85e'
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    if op.get_bind().dialect.name == 'mysql':
+
+        try:
+            op.add_column('tProject', sa.Column('parent_id', sa.Integer(), sa.ForeignKey('tProject.id'), nullable=True))
+        except Exception as e:
+            log.critical(e)
+
+def downgrade():
+    if op.get_bind().dialect.name == 'mysql':
+        try:
+            op.drop_column('tProject', 'parent_id')
+        except Exception as e:
+            log.exception(e)
