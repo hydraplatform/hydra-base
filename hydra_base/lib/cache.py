7a24ce1a5d5ca17f1f8c6e9c5d574efe3defb5a1
Stephen Knox <stephen.knox@manchester.ac.uk>
2022-06-27 23:13:42 +0100

    Fall back to diskcache if pylibmc isn't available. And set the clear_cache correctly for memcache

--- hydra_base/lib/cache.py
+++ hydra_base/lib/cache.py
@@ -5,15 +5,24 @@
     config, in which case use that.
 """
 from hydra_base import config as hydraconfig
-
+import logging
+log = logging.getLogger(__name__)
 global cache
 
 if hydraconfig.get('cache', 'type') is None:
     import diskcache as dc
     cache = dc.Cache('tmp')
 elif hydraconfig.get('cache', 'type') == 'memcached':
-    import pylibmc
-    cache = pylibmc.Client([hydraconfig.get('cache', 'host', '127.0.0.1')], binary=True)
+    try:
+        import pylibmc
+        cache = pylibmc.Client([hydraconfig.get('cache', 'host', '127.0.0.1')], binary=True)
+    except ModuleNotFoundError:
+        log.warning("Unable to find pylibmc. Defaulting to diskcache.")
+        import diskcache as dc
+        cache = dc.Cache('tmp')
 
 def clear_cache():
-    cache.clear()
+    if hasattr(cache, 'flush_all'):
+        cache.flush_all() # memcache
+    else:
+        cache.clear() # diskcache
