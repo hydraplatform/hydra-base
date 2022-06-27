1e15c5bf3bd0ffa8f4a2cc60a659989a8118a014
Stephen Knox <stephen.knox@manchester.ac.uk>
2022-06-27 23:16:42 +0100

    Set the correct tmp dir for diskcache

--- hydra_base/lib/cache.py
+++ hydra_base/lib/cache.py
@@ -4,14 +4,17 @@
     unless 'memcached' is set in the 'cache' section of the
     config, in which case use that.
 """
-from hydra_base import config as hydraconfig
 import logging
+
+from hydra_base import config as hydraconfig
+import tempfile
+
 log = logging.getLogger(__name__)
 global cache
 
 if hydraconfig.get('cache', 'type') is None:
     import diskcache as dc
-    cache = dc.Cache('tmp')
+    cache = dc.Cache(tempfile.gettempdir())
 elif hydraconfig.get('cache', 'type') == 'memcached':
     try:
         import pylibmc
@@ -19,7 +22,7 @@ elif hydraconfig.get('cache', 'type') == 'memcached':
     except ModuleNotFoundError:
         log.warning("Unable to find pylibmc. Defaulting to diskcache.")
         import diskcache as dc
-        cache = dc.Cache('tmp')
+        cache = dc.Cache(tempfile.gettempdir())
 
 def clear_cache():
     if hasattr(cache, 'flush_all'):
