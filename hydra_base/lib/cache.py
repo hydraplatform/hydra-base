90f86e4fb78a13db3d03c2759b6cd220f25b25e7
Stephen Knox <stephen.knox@manchester.ac.uk>
2025-10-02 13:26:25 +0100

    Replace pylibmc with pymemcache

--- hydra_base/lib/cache.py
+++ hydra_base/lib/cache.py
@@ -12,43 +12,41 @@ import tempfile
 log = logging.getLogger(__name__)
 global cache
 
+
 def _init_diskcache():
     log.info("Using diskcache for caching.")
     global cache
     import diskcache as dc
     cache = dc.Cache(tempfile.gettempdir())
 
+
 if hydraconfig.get('cache', 'type') != "memcached":
     _init_diskcache()
-
-elif hydraconfig.get('cache', 'type') == 'memcached':
+else:
     try:
-        import pylibmc
+        from pymemcache.client.base import Client as MemcacheClient
         host = hydraconfig.get('cache', 'host', '127.0.0.1')
-        port = hydraconfig.get('cache', 'port', 31211)
-        cache = pylibmc.Client([f"{host}:{port}"], binary=True)
-
+        port = hydraconfig.get('cache', 'port', 11211)
+        cache = MemcacheClient((host, int(port)))
         # Check if Memcached server is reachable by setting a test key
-        test_key = "__connection_test__"
-        #pick a unique key based on the time
-        test_value = datetime.datetime.toordinal(datetime.datetime.now())
+        test_key = b"__connection_test__"
+        test_value = str(datetime.datetime.toordinal(datetime.datetime.now())).encode()
         try:
-            cache.set(test_key, test_value, 1)
+            cache.set(test_key, test_value, expire=1)
             cache.get(test_key)
             log.info("Connected to memcached server.")
         except Exception:
             raise ConnectionError("Memcached server not responding.")
-
     except (ModuleNotFoundError, ConnectionError) as e:
-        if isinstance(e, ModuleNotFoundError):
-            log.warning("Unable to find pylibmc. Defaulting to diskcache.")
-        else:
-            log.warning("Memcached server not reachable. Defaulting to diskcache.")
-
+        log.warning("Memcached server not reachable or pymemcache not installed. Defaulting to diskcache.")
         _init_diskcache()
 
 def clear_cache():
     if hasattr(cache, 'flush_all'):
-        cache.flush_all() # memcache
-    else:
+        cache.flush_all() # diskcache
+    elif hasattr(cache, 'flush_all'): # legacy memcache
+        cache.flush_all()
+    elif hasattr(cache, 'flush_all_keys'):
+        cache.flush_all_keys() # pymemcache
+    elif hasattr(cache, 'clear'):
         cache.clear() # diskcache
